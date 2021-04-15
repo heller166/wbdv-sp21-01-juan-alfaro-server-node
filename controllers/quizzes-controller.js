e2895d094e2304b8086a635082e5ce8bd808f371
Juan Alfaro <alfaro.j@husky.neu.edu>
2021-04-15 17:30:27 -0400

    Adds mongodb schemas and services for database deployment

--- controllers/quizzes-controller.js
+++ controllers/quizzes-controller.js
@@ -1,24 +1,11 @@
-const quizzesService = require("../services/quizzes/quizzes-service")
+const quizzesService = require('../services/quizzes-service')
 
-module.exports = (app) => {
-    const findAllQuizzes = (req, res) => {
-        const quizzes = quizzesService.findAllQuizzes()
-        res.send(quizzes)
-        //quizzesService.findAllQuizzes()
-        //    .then((quizzes) => {
-        //        res.send(quizzes)
-        //    })
-    }
-    const findQuizById = (req, res) => {
-        const quizId = req.params['qid']
-        const quiz = quizzesService.findQuizById(quizId)
-        res.send(quiz)
-        //quizzesService.findQuizById(quizId)
-        //    .then((quiz) => {
-        //        res.send(quiz)
-        //    })
-    }
+module.exports = function (app) {
+    app.get('/api/quizzes', (req, res) =>
+        quizzesService.findAllQuizzes()
+            .then(allQuizzes => res.json(allQuizzes)))
 
-    app.get("/api/quizzes", findAllQuizzes)
-    app.get("/api/quizzes/:qid", findQuizById)
+    app.get('/api/quizzes/:qzid', (req, res) =>
+        quizzesService.findQuizById(req.params['qzid'])
+            .then(quiz => res.json(quiz)))
 }
